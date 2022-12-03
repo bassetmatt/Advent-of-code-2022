c9e806f821fe1bc47dcb0801795fd9f0b3f833d2
matt <bassetmatt@gmail.com>
2022-12-03 00:55:00 -0500

    [Day 3] Done in python

---
+++ day_03/day_03.py
@@ -0,0 +1,16 @@
+getRef = lambda c: ord('a') if c.islower() else ord('A') - 26
+priorities = [0, 0]
+
+with open('input','r') as f:
+    L = f.read().split('\n')[:-1]
+
+for idx, sack in enumerate(L) :
+    for x in sack[:len(sack)//2] : 
+        if x in sack[len(sack)//2:] :
+            priorities[0] += ord(x) - getRef(x) + 1
+            break
+    for x in sack:
+        if (idx % 3 == 0 and x in L[idx+1] and x in L[idx+2]) :
+            priorities[1] += ord(x) - getRef(x) + 1
+            break
+print(f"Prios part 1 : {priorities[0]}\nPrios part 2 : {priorities[1]}")
