ae86c37e0931972fbe012ffa955b268ab5712345
matt <bassetmatt@gmail.com>
2022-12-04 20:12:22 -0500

    [Day 4] Done in python

---
+++ day_04/four.py
@@ -0,0 +1,8 @@
+import re
+overlaps = [0, 0]
+with open('input','r') as f:
+    for r in ( [int(x) for x in re.search("(\d+)-(\d+),(\d+)-(\d+)",l).groups()] for l in f.read().splitlines() ) : 
+        r1, r2 = set(range(r[0], r[1] + 1)), set(range(r[2], r[3] + 1))
+        overlaps[0] += r1.issubset(r2) or r1.issuperset(r2)
+        overlaps[1] += not r1.isdisjoint(r2)
+print(f"Ranges overlap part 1: {overlaps[0]}\nRanges overlap part 2: {overlaps[1]}")
