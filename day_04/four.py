781f4f383e8e9ecc2239cec24087d11ff81d175a
matt <bassetmatt@gmail.com>
2022-12-06 00:45:17 -0500

    [Day 4] Remove r tag for file

--- day_04/four.py
+++ day_04/four.py
@@ -1,6 +1,6 @@
 import re
 overlaps = [0, 0]
-with open('input','r') as f:
+with open('input') as f:
     for r in ( [int(x) for x in re.search("(\d+)-(\d+),(\d+)-(\d+)",l).groups()] for l in f.read().splitlines() ) : 
         r1, r2 = set(range(r[0], r[1] + 1)), set(range(r[2], r[3] + 1))
         overlaps[0] += r1.issubset(r2) or r1.issuperset(r2)
