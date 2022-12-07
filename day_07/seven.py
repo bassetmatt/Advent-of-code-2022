6c5f7865bd104bd4bf367377e1851f53a01e47db
matt <bassetmatt@gmail.com>
2022-12-07 02:16:29 -0500

    [Day 7] Done in python

---
+++ day_07/seven.py
@@ -0,0 +1,28 @@
+import re
+dirs, CURRENT_PATH = {}, []
+isCommand, pathToString = lambda x : x[0] == '$', lambda x : "/".join(x)[1:]
+class Directory:
+    def __init__(self,path:str) -> None:
+        self.path, self.size, self.children = path, 0, []
+    def getSubSize(self) :
+        self.size += sum([ Directory.getSubSize(dirs[child]) for child in self.children])
+        return self.size
+with open('input') as f:
+    for line in f.read().splitlines():
+        if "$ cd" in line :
+            path = re.search("\$ cd (.+)",line).groups()[0]
+            if path == ".." :
+                CURRENT_PATH.pop()
+            else :
+                CURRENT_PATH.append(path)
+                dirs[pathToString(CURRENT_PATH)] = Directory(pathToString(CURRENT_PATH))
+        elif not "$" in line :
+            if "dir " in line :
+                path = CURRENT_PATH + [re.search("dir (.+)",line).groups()[0]]
+                dirs[pathToString(CURRENT_PATH)].children.append(pathToString(path))
+            else :
+                dirs[pathToString(CURRENT_PATH)].size += int(re.search("(\d+) .+",line).groups()[0])
+used = dirs[''].getSubSize()
+dirs_okay = sorted([d for d in dirs.values() if d.size > used - 40000000],key=lambda x: x.size)
+print(f"Size of files less than 100000 : {sum([ d.size for d in dirs.values() if d.size <= 100000])}")
+print(f"Size of the smallest dir to delete : {dirs_okay[0].size}")
