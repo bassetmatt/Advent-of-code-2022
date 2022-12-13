649db5411b20a1550a7717a5a2281e5845e3bf6a
matt <bassetmatt@gmail.com>
2022-12-13 02:18:30 -0500

    [Day 10] Restore first solution as an other file

---
+++ day_10/ten_first_try.py
@@ -0,0 +1,51 @@
+cycles = 0
+RX = 1
+values = []
+
+interesting = [20 + 40*k for k in range(6)]
+with open('input') as f:
+    X = f.read().splitlines()
+    for i,x in enumerate(X) :
+        if "addx" in x :
+            cycles += 1
+            # print(f"{i:3d} : cycles : {cycles:3d}, RX : {RX:3d} ({x})")
+            if cycles in interesting :
+                #print(RX, cycles, RX*cycles)
+                values.append(RX*cycles)
+            val = int(x[5:])
+            cycles +=1
+            #print(f"{i:3d} : cycles : {cycles:3d}, RX : {RX:3d} ({x})")
+            if cycles in interesting :
+                values.append(RX*cycles)
+            RX += val
+        else :
+            cycles += 1
+            if cycles in interesting :
+                values.append(RX*cycles)
+    print(sum(values))
+def operation(RX,pixels, crt_pos) :
+    if RX - 1 <= crt_pos[1] <= RX + 1:
+        pixels[crt_pos[0]] += '#'
+    else :
+        pixels[crt_pos[0]] += '.'
+
+RX = 1
+cycles = 0
+pixels = ["" for _ in range(6)]
+for i,x in enumerate(X) :
+    if "addx" in x :
+        crt_pos = cycles//40, cycles%40
+        cycles += 1
+        operation(RX,pixels,crt_pos)
+        crt_pos = cycles//40, cycles%40
+        cycles +=1
+        operation(RX,pixels,crt_pos)
+        val = int(x[5:])
+        RX += val
+    else :
+        crt_pos = cycles//40, cycles%40
+        cycles += 1
+        operation(RX,pixels,crt_pos)
+
+for line in pixels:
+    print(line)
